88e21762eaa3483b31517284ceb85dde9def359b
Gustavo Holzmann <86106757+gusholz@users.noreply.github.com>
2024-07-31 15:18:02 +0000

    Removed unused line

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,7 +1,6 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database";
-import db from "node-pg-migrate/dist/db";
 
 export default async function migrations(request, response) {
   const dbClient = await database.getNewClient();
